6e735cb559c99663386d8bcb60b054eebe078240
lucas.henrique.xavier <lucas.h.xavier@gmail.com>
2025-08-09 22:15:56 -0300

    criando metodos para ler e exibir as informações da conta bancaria

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o número do banco:");
+        int numeroBanco = scanner.nextInt();
+
+        System.out.println("Digite o número da agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o nome do Cliente:");
+        String nome = scanner.next();
+
+        System.out.println("Digite o saldo da conta:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroBanco + " e seu saldo " + saldo + " já está disponível para saque."  );
+
     }
 }
